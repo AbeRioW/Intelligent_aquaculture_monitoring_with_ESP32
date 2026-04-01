203e7b0c70336aaa2ecf49c0a8ecb85d3235bf8b
AbeRioW <261507455@qq.com>
2026-04-01 20:20:03 +0800

    ph is ok

--- esp32_oled/esp32_oled.ino
+++ esp32_oled/esp32_oled.ino
@@ -6,6 +6,7 @@
 SSD1306 display(0x3c, 21, 22);
 
 #define TURBIDITY_PIN 35
+#define PH_PIN 34
 
 void setup() {
   display.init();
@@ -26,26 +27,29 @@ int readTurbidity() {
   return analogRead(TURBIDITY_PIN);
 }
 
+int readPH() {
+  return analogRead(PH_PIN);
+}
+
 void loop() {
   float temperature = readTemperature();
   int turbidity = readTurbidity();
+  int phValue = readPH();
   
   display.clear();
   display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 0, "Temperature:");
-  
+  display.drawString(0, 0, "Temperature: ");
   if (temperature == -127.0) {
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 12, "Sensor not found");
+    display.drawString(80, 0, "Sensor not found");
   } else {
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 12, String(temperature) + " °C");
+    display.drawString(80, 0, String(temperature) + " °C");
   }
   
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 28, "Turbidity:");
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 40, String(turbidity) + " ADC");
+  display.drawString(0, 16, "Turbidity: ");
+  display.drawString(80, 16, String(turbidity) + " ADC");
+  
+  display.drawString(0, 32, "PH Value: ");
+  display.drawString(80, 32, String(phValue) + " ADC");
   
   display.display();
   delay(1000);
